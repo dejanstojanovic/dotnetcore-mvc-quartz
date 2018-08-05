f5df8827716856c6182b3c75c4eb6cc65f9102ab
dejanstojanovic <d.stojanovic@hotmail.com>
2018-08-05 15:53:22 +0400

    Initial commit

---
+++ Schedule.WebApiCore.Sample/Schedule/ScheduledJob.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.WebApiCore.Sample.Schedule
+{
+    public class ScheduledJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+
+
+            //await Task.FromResult("");
+            await Task.CompletedTask;
+            
+        }
+    }
+}
---
+++ Schedule.WebApiCore.Sample/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
+using Schedule.WebApiCore.Sample.Schedule;
+
+namespace Schedule.WebApiCore.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(provider =>
+            {
+                var schedulerFactory = new StdSchedulerFactory();
+                var scheduler = schedulerFactory.GetScheduler();
+
+                IJobDetail job = JobBuilder.Create<ScheduledJob>()
+                    .WithIdentity("FiveSecondsJob", "SampleJobs") // name "myJob", group "group1"
+                    .Build();
+
+                ITrigger trigger = TriggerBuilder.Create()
+                 .WithSimpleSchedule
+                  (s =>
+                     s.WithInterval(TimeSpan.FromSeconds(5))
+                     .RepeatForever()
+                  )
+                .Build();
+
+                scheduler.
+                //scheduler..(job, trigger);
+
+                scheduler.Start();
+                return scheduler;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
